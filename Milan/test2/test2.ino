e36a3227ea6a91797881ff99017e8d81d61bd7e6
UAFS <ssdontletmed@gmail.com>
2024-07-25 13:06:14 -0500

    changes were made to test 2 uno code

--- Milan/test2/test2.ino
+++ Milan/test2/test2.ino
@@ -6,10 +6,10 @@ Servo motorServo;  // Servo that controls the speed of the motor
 
 // Enum for different actions
 enum RobotAction {
-  Alignmentmv,
-  AvoidObstacle,
-  Stop,
-  Forward
+  Alignmentmv = 1,
+  AvoidObstacle = 2,
+  Stop = 3,
+  Forward = 4
 };
 
 // Function declaration for performing actions based on the enum
@@ -25,7 +25,6 @@ void setup() {
 
   // Start serial communication at 9600 baud rate
   Serial.begin(9600);
-  Serial1.begin(9600);
 
   // Set the ESC to the neutral state
   motorServo.write(90); // 90 degrees corresponds to a neutral signal (1500 microsecond pulse width)
@@ -34,20 +33,20 @@ void setup() {
 
 void loop() {
   // Example usage: perform different actions based on the received signal
-  if (Serial1.available() > 0) {
-    String value = Serial1.readStringUntil('\n');
+  if (Serial.available() > 0) {
+    String value = Serial.readStringUntil('\n');
     int action = value.toInt();
     switch (action) {
-      case 1:
+      case Alignmentmv:
         performAction(Alignmentmv);
         break;
-      case 2:
+      case AvoidObstacle:
         performAction(AvoidObstacle);
         break;
-      case 3:
+      case Stop:
         performAction(Stop);
         break;
-      case 4:
+      case Forward:
         performAction(Forward);
         break;
       default:
@@ -58,6 +57,8 @@ void loop() {
 
 void performAction(RobotAction action) {
   switch (action) {
+
+    //ANOTHERCASE  
     case Alignmentmv:
       if (Serial.available() > 0) {
         // Perform Alignmentmv action
@@ -73,29 +74,39 @@ void performAction(RobotAction action) {
         delay(40);
       }
       break;
+
+    //ANOTHERCASE  
     case AvoidObstacle:
       // Perform avoid obstacle action
-      int val = 58;
-      int pos = map(val, 0, 100, 10, 180); // 95 is the zero position of 50 speed
-      motorServo.write(pos);               // Write the mapped position to the motor servo
-      wangleServo.write(115);
-      delay(4000);
-      motorServo.write(90);
-      wangleServo.write(55);
-      motorServo.write(pos);
-      delay(10000);
+      {
+        int val = 58;
+        int pos = map(val, 0, 100, 10, 180); // 95 is the zero position of 50 speed
+        motorServo.write(pos);               // Write the mapped position to the motor servo
+        wangleServo.write(115);
+        delay(4000);
+        motorServo.write(90);
+        wangleServo.write(55);
+        motorServo.write(pos);
+        delay(10000);
+      }
       break;
+      
+    //ANOTHERCASE  
     case Stop:
       // Perform stop action
       motorServo.write(90); // Stop the motor
       break;
+      
+    //ANOTHERCASE  
     case Forward:
       // Moving forward
-      val = 58;
-      pos = map(val, 0, 100, 10, 180);
-      motorServo.write(pos);
-      wangleServo.write(90);
-      delay(50);
+      {
+        int val = 58;
+        int pos = map(val, 0, 100, 10, 180);
+        motorServo.write(pos);
+        wangleServo.write(90);
+        delay(50);
+      }
       break;
   }
 }
