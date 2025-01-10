eb4a93b7f6bb057c1aa95a14648ffd4c0bfb33fd
Manuel <manuelmartinezint1@gmail.com>
2025-01-09 23:18:40 -0600

    all updates to the milian files im planing on using for the comp and orgnaixing folders

--- Res512/Milian/AI_test_one.py
+++ Res512/Milian/AI_test_one.py
@@ -1,8 +1,8 @@
 import jetson.inference
 import jetson.utils
 
-net = jetson.inference.detectNet(model="/home/uafs/Downloads/jetson-inference/python/training/detection/ssd/models/test_done/ssd-mobilenet.onnx",
-                                 labels="/home/uafs/Downloads/jetson-inference/python/training/detection/ssd/models/test_done/labels.txt",
+net = jetson.inference.detectNet(model="/home/uafs/Downloads/jetson-inference/python/training/detection/ssd/models/test_eone/ssd-mobilenet.onnx",
+                                 labels="/home/uafs/Downloads/jetson-inference/python/training/detection/ssd/models/test_eone/labels.txt",
                                  input_blob="input_0",
                                  output_cvg="scores",
                                  output_bbox="boxes",
@@ -13,14 +13,23 @@ camera = jetson.utils.videoSource("/dev/video0")  # Camera input
 display = jetson.utils.videoOutput()  # Display output
 
 while True:
-    # Capture image from the camera
     img = camera.Capture()
-
-    # Run detection on the image
     detections = net.Detect(img)
-
-    # Render the image to display
     display.Render(img)
 
-    # Display the FPS in the status bar
+    width = img.width
+    height = img.height
+
+    if detections:
+        for detect in detections:
+            ID = detect.ClassID
+            top = int(detect.Top)
+            left = int(detect.Left)
+            bottom = int(detect.Bottom)
+            right = int(detect.Right)
+            item = net.GetClassDesc(ID)
+            w = right - left
+            print(f'Width of object: {w}')  # Print error value for debugging
+
+
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
