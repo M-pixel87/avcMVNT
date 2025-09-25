29ee04f4585956b653a0cf3182080c710362961f
Hootsoon <hootsoon2@gmail.com>
2025-09-25 11:59:45 -0500

    Display system updated for video display and organization of universalMain

--- Milian_dsummer25/UniversalSystem/Systems/InputSystem.py
+++ Milian_dsummer25/UniversalSystem/Systems/InputSystem.py
@@ -1,10 +1,32 @@
 import os
 import pygame
 from Arm import CoOrdinateBaseSys as Arm
+import jetson_utils
 
 
 # headless support (Jetson/Ubuntu without display)
 os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+class webcam:
+    def __init__(self, cam_id=0, width=640, height=480):
+        self.cam_id = cam_id
+        self.width = width
+        self.height = height
+        camera = jetson_utils.videoSource("/dev/video[{}]".format(cam_id), argv=["--input-width={}".format(width), "--input-height={}".format(height)])
+        display = jetson_utils.videoOutput("display://0", argv=["--output-width={}".format(width), "--output-height={}".format(height)])
+
+    def get_frame(self):
+        img = self.camera.Capture()
+        return img
+    
+    def show_frame(self, img):
+        self.display.Render(img)
+        self.display.SetStatus("Object Detection | Network {:.0f} FPS".format(self.network.GetNetworkFPS()))
+
+    def release(self):
+        self.camera.Close()
+        self.display.Close()
+
 
 
 class XboxController:
--- Milian_dsummer25/UniversalSystem/universalMain.py
+++ Milian_dsummer25/UniversalSystem/universalMain.py
@@ -10,30 +10,34 @@ PORT = "/dev/ttyACM0"
 BAUD = 9600
 ser = serial.Serial(PORT, BAUD, timeout=1)
 
+pygame.init()
+pygame.joystick.init()
+controller = XboxController()
+motors = CytronMotor(in1=4, an1=5, in2=7, an2=6, ser=ser)
+display = DisplaySystem()
 
 def main():
-    pygame.init()
-    pygame.joystick.init()
-    controller = XboxController()
-    motors = CytronMotor(in1=4, an1=5, in2=7, an2=6, ser=ser)
-    display = DisplaySystem()
     try:
         while True:
-            pygame.event.pump()
-            ctrl_data = controller.poll()
-            # Get raw axes for display
-            joystick = controller.joystick
-            axes = [joystick.get_axis(i) for i in range(joystick.get_numaxes())]
-            print(ctrl_data)
-            motors.set_power(ctrl_data["L"], ctrl_data["R"])
-            display.update_display(
-                axes,  # show all axes
-                ctrl_data["buttons"],
-                (ctrl_data["L"], ctrl_data["R"])
-            )
+            pygameHandle()
             time.sleep(0.1)
     except KeyboardInterrupt:
         print("Stopping...")
+
+def pygameHandle():
+     # MAIN PYGAME EVENT PROCESSING : CONTROLLER INPUTS
+    pygame.event.pump()
+    ctrl_data = controller.poll()
+    # Get raw axes for display
+    joystick = controller.joystick
+    axes = [joystick.get_axis(i) for i in range(joystick.get_numaxes())]
+    print(ctrl_data)
+    motors.set_power(ctrl_data["L"], ctrl_data["R"])
+    display.update_display(
+        axes,  # show all axes
+        ctrl_data["buttons"],
+        (ctrl_data["L"], ctrl_data["R"])
+    )
 
 if __name__ == "__main__":
     main()
