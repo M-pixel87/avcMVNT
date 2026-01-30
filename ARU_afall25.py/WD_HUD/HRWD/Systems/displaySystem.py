dcacda527866d7156d58ed5055dc6c1e6a9d9385
Hudson <hootsoon2@gmail.com>
2026-01-30 15:10:14 -0600

    updated ai, working intel cam , optimizations, multithreaded cameras

--- ARU_afall25.py/WD_HUD/HRWD/Systems/displaySystem.py
+++ ARU_afall25.py/WD_HUD/HRWD/Systems/displaySystem.py
@@ -67,7 +67,7 @@ class DisplaySystem_YOLO:
                 conf = det["confidence"]
 
                 # Draw bounding box
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
                 text = f"{label} {conf:.2f}"
                 cv2.putText(frame, text, (x1, y1 - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
