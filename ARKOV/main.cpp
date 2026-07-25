daca0007c7dced695e736a9f1402325011158242
Hootsoon <hootsoon2@gmail.com>
2026-07-25 12:46:17 -0500

    add all imports to main

--- ARKOV/main.cpp
+++ ARKOV/main.cpp
@@ -6,11 +6,22 @@ c++ systems.
 #include "types.hh"
 #include "microControllerCom.h"
 #include "pidController.hh"
+#include "slamManager.hh"
+#include "voxel_map.hh"
+#include "lidar.hh"
+#include "particleFilter.hh"
+#include "driveOdometry.hh"
+
 
 
 microcontroller stm32(); // create serial com with stm32
 pidController leftPID(0.30, 0.05, 0.10); // create pid controller for left motor
 pidController rightPID(0.30, 0.05, 0.10); // create pid controller for right motor
+voxel_map map(); // Create 3d voxel map representation
+lidar ld(); // lidar for unitree 4d l2
+particleFilter pf(); // Create particle filter for localization
+driveOdometry driveOdom(); // Drive odometry for counting tics
+slamManager slamMan(&pf, &driveOdom, &map, &ld); // create and pass references to slam classes
 
 
 int main(){
--- ARKOV/slam/slamManager.cpp
+++ ARKOV/slam/slamManager.cpp
@@ -1,8 +1,7 @@
 #include "slamManager.hh"
 
 // Constructor takes pointers to the components it needs to coordinate
-slamManager::slamManager(particleFilter* p_filter, driveOdometry* d_odom, 
-                         VoxelMap* v_map, Lidar* l_sensor) {
+slamManager::slamManager(particleFilter* p_filter, driveOdometry* d_odom, VoxelMap* v_map, Lidar* l_sensor) {
     pf = p_filter;
     driveOdom = d_odom;
     map = v_map;
@@ -20,8 +19,7 @@ void slamManager::startUp() {
     for (const auto& pt : scan) {
         // You'll need to transform the raw point based on bestP (like we did in updateWeights)
         // Then pass the sensor origin and the point endpoint to Bresenham
-        map->bresenham3D(bestP.x, bestP.y, SENSOR_Z_OFFSET,  // Ray start (LiDAR position)
-                         pt.world_x, pt.world_y, pt.world_z); // Ray end (Hit point)
+        map->bresenham3D(bestP.x, bestP.y, SENSOR_Z_OFFSET, pt.world_x, pt.world_y, pt.world_z); // Ray end (Hit point)
     }
 }
 
